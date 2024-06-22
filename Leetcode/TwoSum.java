72b96ab779b61526d3640cc997b24c05102a0692
Keshav Sharma <sharmakeshav2603@gmail.com>
2024-06-22 17:41:54 +0530

    Update TwoSum.java

--- Leetcode/TwoSum.java
+++ Leetcode/TwoSum.java
@@ -3,11 +3,11 @@ public class TwoSum {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
+                    return new int[] { i, j };//use this way of returning array
                 }
             }
         }
-        return new int[0];
+        return new int[0];//returing a empty array
     }
 
 
