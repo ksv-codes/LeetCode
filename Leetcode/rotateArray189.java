87bbbfdf7ca8585f0d9033d304c804e31ec71aeb
Keshav Sharma <sharmakeshav2603@gmail.com>
2024-06-25 17:43:52 +0530

    Update rotateArray189.java

--- Leetcode/rotateArray189.java
+++ Leetcode/rotateArray189.java
@@ -5,18 +5,18 @@ public class rotateArray189 {
             System.out.print(i+" ");
         }
         System.out.println();
-        int[] res = new int[7];
-        int n = 7;
+        int[] res = new int[arr.length];
+        int n = arr.length;
         int k = 5;
         //{3,4,5,6,7,1,2}
-        for (int i = (7-5); i < 7; i++) {
+        for (int i = (n-k); i < n; i++) {
             for (int j = i-2; j < i-1; j++) {
                 res[j]=arr[i];
                 
             }
         }
-        for (int i = 0; i < 2; i++) {//i=0,1
-            for (int j = i+5; j<i+6 ; j++) {//j=6,7
+        for (int i = 0; i < n-k; i++) {//i=0,1
+            for (int j = i+k; j<i+(k+1) ; j++) {//j=5,6
                 res[j]=arr[i];
 
             }
